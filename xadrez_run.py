00ebccd1507949ada483ffd1a03fb95b6ee30a7e
jabomgo <jonathan.neves@dcx.ufpb.br>
2024-07-10 17:31:11 -0300

    Objetos em tabuleiro

--- xadrez_run.py
+++ xadrez_run.py
@@ -1,8 +1,11 @@
 tabuleiro = [[0 for _ in range(8)] for _ in range(8)]
 
 def mostra_tabuleiro():
-    for linha in tabuleiro:
-        print(linha)
+    for linha in range(tabuleiro.__len__()):
+        formater = []
+        for coluna in range(tabuleiro.__len__()):
+            formater.append(tabuleiro[linha][coluna].__str__())
+        print(formater)
 
 class Peca:
     def __init__(self, cor, linha_atual, coluna_atual):
@@ -42,12 +45,19 @@ class Cavalo(Peca):
     def __str__(self) -> str:
         return 'Cavalo '+ self.cor
     
-class Rainha(Peca):
+class Dama(Peca):
     def movimento_valido(self, linha_destino, coluna_destino):
         return super().movimento_valido(linha_destino, coluna_destino)
     
     def __str__(self) -> str:
-        return 'Rainha '+ self.cor
+        return 'Dama '+ self.cor
+    
+class Bispo(Peca):
+    def movimento_valido(self, linha_destino, coluna_destino):
+        return super().movimento_valido(linha_destino, coluna_destino)
+    
+    def __str__(self) -> str:
+        return 'Bispo '+ self.cor
     
 def preparar_tabuleiro():
     #preparar a logica para colocar todas as peças nos lugares corretos
@@ -56,47 +66,41 @@ def preparar_tabuleiro():
         #tabuleiro acima
         if linha == 1:
             for coluna in range(tabuleiro.__len__()):
-                tabuleiro[linha][coluna] = 'peao'
+                tabuleiro[linha][coluna] = Peao('branco', linha, coluna)
         #tabuleiro baixo
         if linha == 6:
             for coluna in range(tabuleiro.__len__()):
-                tabuleiro[linha][coluna] = 'peao'
+                tabuleiro[linha][coluna] = Peao('preto', linha, coluna)
     
     #demais peças colocar aqui com a posição correta manualmente
 
-    tabuleiro[0][0] = 'torre'
-    tabuleiro[0][7] = 'torre'
+    tabuleiro[0][0] = Torre('branco', 0, 0)
+    tabuleiro[0][7] = Torre('branco', 0, 7)
 
-    tabuleiro[0][1] = 'cavalo'
-    tabuleiro[0][6] = 'cavalo'
+    tabuleiro[0][1] = Cavalo('branco', 0, 1)
+    tabuleiro[0][6] = Cavalo('branco', 0, 6)
 
-    tabuleiro[0][2] = 'bispo'
-    tabuleiro[0][5] = 'bispo'
+    tabuleiro[0][2] = Bispo('branco', 0, 2)
+    tabuleiro[0][5] = Bispo('branco', 0, 5)
 
-    tabuleiro[0][3] = 'rei'
-    tabuleiro[0][4] = 'dama'
+    tabuleiro[0][3] = Rei('branco', 0,3)
+    tabuleiro[0][4] = Dama('branco', 0,4)
 
     #outro lado
 
-    tabuleiro[7][0] = 'torre'
-    tabuleiro[7][7] = 'torre'
+    tabuleiro[7][0] = Torre('preto', 7, 0)
+    tabuleiro[7][7] = Torre('preto', 7, 7)
 
-    tabuleiro[7][1] = 'cavalo'
-    tabuleiro[7][6] = 'cavalo'
+    tabuleiro[7][1] = Cavalo('preto', 7, 1)
+    tabuleiro[7][6] = Cavalo('preto', 7, 6)
 
-    tabuleiro[7][2] = 'bispo'
-    tabuleiro[7][5] = 'bispo'
+    tabuleiro[7][2] = Bispo('preto', 7,2)
+    tabuleiro[7][5] = Bispo('preto', 7,5)
 
-    tabuleiro[7][3] = 'rei'
-    tabuleiro[7][4] = 'dama'
+    tabuleiro[7][3] = Rei('preto', 7,3)
+    tabuleiro[7][4] = Dama('preto', 7, 4)
 
 # main game
 
 preparar_tabuleiro()
 mostra_tabuleiro()
-
-peao = Peao('branco', 1,2)
-
-rainha = Rainha('preto', 1, 1)
-print(peao)
-print(rainha)
